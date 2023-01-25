b975549afc782bc8834d3eb19b06fcd979fd17f3
AkashPanda1701 <akashpanda1701@gmail.com>
2023-01-26 00:38:44 +0530

    all routes done

---
+++ src/config/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+const connectDB = async () => {
+
+    return mongoose.connect(process.env.DB_URL);
+};
+
+
+module.exports = connectDB;
